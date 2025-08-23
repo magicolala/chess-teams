09a60311c8ab2fb6133f68d1fb15aa2479eac0b9
Cédric Oloa <oloa.cedric.bts@gmail.com>
2025-08-23 09:01:45 +0200

    feat: refactor l'entité TeamMember pour utiliser UUID et ajouter des méthodes associées

--- src/Entity/TeamMember.php
+++ src/Entity/TeamMember.php
@@ -6,15 +6,72 @@ use App\Repository\TeamMemberRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TeamMemberRepository::class)]
+#[ORM\UniqueConstraint(name: 'uniq_team_user', columns: ['team_id', 'user_id'])]
 class TeamMember
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    #[ORM\Column(type: 'uuid', unique: true)]
+    private string $id;
 
-    public function getId(): ?int
+    #[ORM\ManyToOne(targetEntity: Team::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private Team $team;
+
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private User $user;
+
+    #[ORM\Column(type: 'integer')]
+    private int $position = 0;
+
+    #[ORM\Column(type: 'boolean')]
+    private bool $active = true;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private \DateTimeImmutable $joinedAt;
+
+    public function __construct(Team $team, User $user, int $position)
+    {
+        $this->id = \Symfony\Component\Uid\Uuid::v4()->toRfc4122();
+        $this->team = $team;
+        $this->user = $user;
+        $this->position = $position;
+        $this->joinedAt = new \DateTimeImmutable();
+        $this->active = true;
+    }
+
+    public function getId(): string
     {
         return $this->id;
+    }
+    public function getTeam(): Team
+    {
+        return $this->team;
+    }
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+    public function getPosition(): int
+    {
+        return $this->position;
+    }
+    public function setPosition(int $p): self
+    {
+        $this->position = $p;
+        return $this;
+    }
+    public function isActive(): bool
+    {
+        return $this->active;
+    }
+    public function setActive(bool $a): self
+    {
+        $this->active = $a;
+        return $this;
+    }
+    public function getJoinedAt(): \DateTimeImmutable
+    {
+        return $this->joinedAt;
     }
 }
