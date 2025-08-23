518b453ea07824a57a1630a12ab33d940c218f48
Cédric Oloa <oloa.cedric.bts@gmail.com>
2025-08-23 09:11:00 +0200

    feat: refactor l'entité Invite en Move pour utiliser UUID et ajouter des propriétés et des méthodes associées

--- src/Entity/Invite.php
+++ src/Entity/Invite.php
@@ -2,19 +2,93 @@
 
 namespace App\Entity;
 
-use App\Repository\InviteRepository;
+use App\Repository\MoveRepository;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: InviteRepository::class)]
-class Invite
+#[ORM\Entity(repositoryClass: MoveRepository::class)]
+#[ORM\Index(columns: ['game_id', 'ply'])]
+class Move
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    public const TYPE_NORMAL = 'normal';
+    public const TYPE_TIMEOUT = 'timeout-pass';
 
-    public function getId(): ?int
+    #[ORM\Id]
+    #[ORM\Column(type: 'uuid', unique: true)]
+    private string $id;
+
+    #[ORM\ManyToOne(targetEntity: Game::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private Game $game;
+
+    #[ORM\ManyToOne(targetEntity: Team::class)]
+    private ?Team $team = null;
+
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    private ?User $byUser = null;
+
+    #[ORM\Column(type: 'integer')]
+    private int $ply;
+
+    #[ORM\Column(type: 'string', nullable: true)]
+    private ?string $uci = null;
+
+    #[ORM\Column(type: 'string', nullable: true)]
+    private ?string $san = null;
+
+    #[ORM\Column(type: 'text')]
+    private string $fenAfter;
+
+    #[ORM\Column(length: 20)]
+    private string $type = self::TYPE_NORMAL;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private \DateTimeImmutable $createdAt;
+
+    public function __construct(Game $g, int $ply)
+    {
+        $this->id = \Symfony\Component\Uid\Uuid::v4()->toRfc4122();
+        $this->game = $g;
+        $this->ply = $ply;
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+    // Getters/Setters (minimaux)
+    public function getId(): string
     {
         return $this->id;
+    }
+    public function getGame(): Game
+    {
+        return $this->game;
+    }
+    public function setTeam(?Team $t): self
+    {
+        $this->team = $t;
+        return $this;
+    }
+    public function setByUser(?User $u): self
+    {
+        $this->byUser = $u;
+        return $this;
+    }
+    public function setUci(?string $u): self
+    {
+        $this->uci = $u;
+        return $this;
+    }
+    public function setSan(?string $s): self
+    {
+        $this->san = $s;
+        return $this;
+    }
+    public function setFenAfter(string $f): self
+    {
+        $this->fenAfter = $f;
+        return $this;
+    }
+    public function setType(string $t): self
+    {
+        $this->type = $t;
+        return $this;
     }
 }
