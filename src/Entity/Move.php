e36eabbf2691b05887986a8dca641688633c4163
Cédric Oloa <oloa.cedric.bts@gmail.com>
2025-08-23 08:51:17 +0200

    feat: ajouter les entités Game, Invite, Move, Team, TeamMember et leurs dépôts associés

---
+++ src/Entity/Move.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MoveRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MoveRepository::class)]
+class Move
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
