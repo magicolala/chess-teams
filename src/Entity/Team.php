22c5bb66b8291d420c32c10fc446b1949caacca6
Cédric Oloa <oloa.cedric.bts@gmail.com>
2025-08-23 09:00:50 +0200

    feat: refactor l'entité Team pour utiliser UUID et ajouter des méthodes associées

--- src/Entity/Team.php
+++ src/Entity/Team.php
@@ -6,15 +6,52 @@ use App\Repository\TeamRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TeamRepository::class)]
+#[ORM\UniqueConstraint(name: 'uniq_game_name', columns: ['game_id', 'name'])]
 class Team
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    public const NAME_A = 'A';
+    public const NAME_B = 'B';
 
-    public function getId(): ?int
+    #[ORM\Id]
+    #[ORM\Column(type: 'uuid', unique: true)]
+    private string $id;
+
+    #[ORM\ManyToOne(targetEntity: Game::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private Game $game;
+
+    #[ORM\Column(length: 1)]
+    private string $name;
+
+    #[ORM\Column(type: 'integer')]
+    private int $currentIndex = 0;
+
+    public function __construct(Game $game, string $name)
+    {
+        $this->id = \Symfony\Component\Uid\Uuid::v4()->toRfc4122();
+        $this->game = $game;
+        $this->name = $name;
+    }
+
+    public function getId(): string
     {
         return $this->id;
+    }
+    public function getGame(): Game
+    {
+        return $this->game;
+    }
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    public function getCurrentIndex(): int
+    {
+        return $this->currentIndex;
+    }
+    public function setCurrentIndex(int $i): self
+    {
+        $this->currentIndex = $i;
+        return $this;
     }
 }
