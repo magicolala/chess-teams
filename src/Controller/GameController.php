37035ea4c645cd8b7015fc9c1f286d6bfb690597
Cédric Oloa <16673302+magicolala@users.noreply.github.com>
2025-10-03 15:05:24 +0200

    Update routing attributes for Symfony 7

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -33,7 +33,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mercure\HubInterface;
 use Symfony\Component\Mercure\Update;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/games', name: 'game_')]
 final class GameController extends AbstractController
